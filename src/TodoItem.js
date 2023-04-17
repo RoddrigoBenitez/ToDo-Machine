8a93bd6643c631290ecfda9c0d9102d56ecb9867
unknown <roddrigo.benitez@gmail.com>
2023-04-17 16:11:33 -0300

    class extends

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,28 @@
 import React from "react";
 import './todoStyle.css';
 
+
+
 const TodoItem = (props) => {
+    const onCompleted = () => {
+        alert('Se completo esto! ' + props.text);
+    }
+    
+    
+    const onDelete = () => {
+        alert('Borraste este ' + props.text);
+    }
+    
+
+
+
     return(
         <li className="todoItem">
-            <span  className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
+            <span  className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+            onClick={onCompleted}>√</span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
-            <span className="Icon Icon-delete">X</span>
+            <span className="Icon Icon-delete"
+            onClick={onDelete}>X</span>
         </li>
     )
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,9 +1,15 @@
 import React from "react";
 import './todoStyle.css';
 
+
+
 const TodoSearch = () => {
+    const onSearchValue = (event) =>{
+        console.log(event.target.value);
+    }
+
     return(
-        <input className="todoSearch" placeholder='Buscar' />
+        <input className="todoSearch" placeholder='Buscar' onChange={onSearchValue} />
     )
 }
 
